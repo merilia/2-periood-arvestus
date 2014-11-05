a9ace44d3b77b2bbcb637b029193cecc4a40957e
Merili <meriliasu@gmail.com>
2014-11-05 10:31:52 +0200

    Initial commit
    Initial commit

---
+++ ylesanne-1/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>II perioodi arvestustöö - Ülesanne 1 - GET päring</title>
+  </head>
+
+  <body>
+  	<?php $username= "Merili"; $age = "21"; ?>
+  	<a href="result.php">Teine leht</a>
+  </body>
+</html>
